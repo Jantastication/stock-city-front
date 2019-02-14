f8e3c013dd2bf56823ea365b7ea16f322909e505
github username <github email address>
2019-02-14 12:23:22 -0600

    components built

--- src/App.js
+++ src/App.js
@@ -1,26 +1,101 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+// import { HomePage } from "./views/HomePage";
+import { Homepage } from "./components/Homepage";
+import {
+  BrowserRouter,
+  Route,
+  Switch,
+  withRouter,
+  Redirect
+} from "react-router-dom";
+import { NavBar } from "./components/NavBar";
+import { Login } from "./components/Login";
+import { Portfolio } from "./components/Portfolio";
+import { SignUp } from "./components/SignUp";
+import { PrivateRoute } from "./components/PrivateRoute";
 
 class App extends Component {
+  state = {
+    currentPrices: [],
+    currentUser: null
+  };
+
+  componentDidMount() {
+    let userID = localStorage.getItem("userID");
+    if (userID) {
+      // Fetch user
+      fetch(`https://tradeup-api.herokuapp.com/api/v1/users/${userID}`, {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`
+        }
+      })
+        .then(res => res.json())
+        .then(currentUser => {
+          this.setState({ currentUser });
+        });
+    }
+    this.fetchCurrentPrice();
+    setInterval(this.fetchCurrentPrice, 30000);
+  }
+
+  setCurrentUser = currentUser => {
+    this.setState({ currentUser });
+  };
+
+  fetchCurrentPrice = companySymbol => {
+    fetch(
+      "https://api.iextrading.com/1.0//stock/market/batch?symbols=aapl,fb,tsla,ba,brk.b,dis,ge,hd,nke,sbux,dji,amzn,baba,goog,nflx,adbe,ftnt,grub,irbt,mcd&types=chart&range=1d"
+    )
+      .then(res => res.json())
+      .then(prices => {
+        for (let symbol in prices) {
+          let chart = prices[symbol].chart;
+          prices[symbol] = chart[chart.length - 1];
+        }
+        this.setState({ currentPrices: prices });
+      });
+  };
+
   render() {
+    console.log("app", this.props.history);
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <NavBar />
+          <Switch>
+            <Route
+              exact
+              path="/"
+              render={() =>
+                localStorage.userID ? <Redirect to="/home" /> : <Login />
+              }
+            />
+            {/* <Route path="/logout" component={Logout} /> */}
+            <PrivateRoute
+              path="/home"
+              render={props => (
+                <Homepage {...props} currentPrices={this.state.currentPrices} />
+              )}
+            />
+            <PrivateRoute
+              path="/Portfolio"
+              render={props => (
+                <Portfolio
+                  {...props}
+                  currentPrices={this.state.currentPrices}
+                />
+              )}
+            />
+            <Route
+              path="/SignUp"
+              render={() =>
+                localStorage.userID ? <Redirect to="/home" /> : <SignUp />
+              }
+            />
+          </Switch>
+        </div>
+      </BrowserRouter>
     );
   }
 }
---
+++ src/components/PrivateRoute.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Redirect, Route, withRouter } from "react-router-dom";
+
+const _PrivateRoute = ({ component: Component, render, ...rest }) => (
+  <Route
+    {...rest}
+    render={props =>
+      localStorage.getItem("token") ? (
+        Component ? (
+          <Component {...props} />
+        ) : (
+          render(props)
+        )
+      ) : (
+        <Redirect to={{ pathname: "/", state: { from: props.location } }} />
+      )
+    }
+  />
+);
+
+export const PrivateRoute = withRouter(_PrivateRoute);
